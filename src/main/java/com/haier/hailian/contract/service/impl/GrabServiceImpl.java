098252f6f002d63814d4443912c0cd849ec15d70
19012964 <panyulong@haier.com>
2019-12-17 17:06:12 +0800

    [fix]添加Service注解

--- src/main/java/com/haier/hailian/contract/service/impl/GrabServiceImpl.java
+++ src/main/java/com/haier/hailian/contract/service/impl/GrabServiceImpl.java
@@ -5,12 +5,14 @@ import com.haier.hailian.contract.dto.grab.MeshStatisticQueryDto;
 import com.haier.hailian.contract.dto.grab.MeshSummaryDto;
 import com.haier.hailian.contract.dto.grab.TyMasterGrabChainInfoDto;
 import com.haier.hailian.contract.service.GrabService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by 19012964 on 2019/12/17.
  */
+@Service
 public class GrabServiceImpl implements GrabService {
     @Override
     public TyMasterGrabChainInfoDto queryChainInfo(MeshStatisticQueryDto queryDto) {
