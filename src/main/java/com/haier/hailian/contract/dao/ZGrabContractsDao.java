1e8074bb795807cde3d2913c40eb67e61dbf3001
HAIER\19033715 <liuyuqi@haier.com>
2019-12-18 17:48:35 +0800

    生成抢单实体类 + mapper

---
+++ src/main/java/com/haier/hailian/contract/dao/ZGrabContractsDao.java
@@ -0,0 +1,16 @@
+package com.haier.hailian.contract.dao;
+
+import com.haier.hailian.contract.entity.ZGrabContracts;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 19033715
+ * @since 2019-12-18
+ */
+public interface ZGrabContractsDao extends BaseMapper<ZGrabContracts> {
+
+}
