efb271b98c7ff4d6a9b4dcaed04509b4cd121ec4
HAIER\19033715 <liuyuqi@haier.com>
2019-12-18 15:04:10 +0800

    备注

--- src/main/java/com/haier/hailian/contract/service/homepage/impl/HomePageImpl.java
+++ src/main/java/com/haier/hailian/contract/service/homepage/impl/HomePageImpl.java
@@ -23,6 +23,11 @@ public class HomePageImpl implements HomePageService {
     public List<Object> getContractList(ContractListsDto contractListsDto) {
         List<Object> list = new ArrayList<>();
         try{
+            // 1. z_grab_contracts 获取抢单信息  parent_id 合约id
+            // 2. 通过合约id 去 z_gambling_contracts 获取链群编码  chain_code
+            // 3. 通过chain_code 去 z_hr_chain_info获取链群信息
+            // 4. 通过前端月份时间 + 通过chain_code（链群编码） 去 z_target_basic获取对应月份预计
+            // 5. 实际 - 现在卡住了（大数据现在需要给我们数据）
             list = null;
         }catch (Exception e){
             e.printStackTrace();
@@ -40,10 +45,12 @@ public class HomePageImpl implements HomePageService {
     public Map<String, Object> getChainGroupInfo(ChainGroupInfoDto chainGroupInfoDto) {
         Map<String, Object> map = new HashMap<>();
         try{
-            // 链群等级
+            // 链群等级  现在规则没出来  卡住了
             map.put("chainGroupRank","");
-            // 链群、节点、人的增值分享
-            map.put("share","");
+            // 链群的增值分享   实际 - 没有  卡住了
+            map.put("chainShare","");
+            // 节点、人的增值分享  实际 - 没有  卡住了
+            map.put("nodeShare","");
             // 吸引力预计
             map.put("estimate","");
             // 吸引力实际
