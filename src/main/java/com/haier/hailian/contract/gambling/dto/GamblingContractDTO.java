de62f8a12984477f94ff66fa8cebd4bb030e6676
01431594 <S2LTshRZbaed1gk>
2019-12-19 10:39:25 +0800

    链群主举单接口

---
+++ src/main/java/com/haier/hailian/contract/gambling/dto/GamblingContractDTO.java
@@ -0,0 +1,72 @@
+package com.haier.hailian.contract.gambling.dto;
+
+import com.haier.hailian.contract.entity.ZGamblingContractsProductIndex;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by 01431594 on 2019/12/19.
+ */
+@Data
+public class GamblingContractDTO {
+
+    private Integer id;
+    /**
+     * 合约名称
+     */
+    private String contractsName;
+
+    /**
+     * 链群编码
+     */
+    private String chainCode;
+    /**
+     * 链群名称
+     */
+    private String chainName;
+
+    /**
+     * 开始时间
+     */
+    private LocalDateTime startDate;
+
+    /**
+     * 结束时间
+     */
+    private LocalDateTime endDate;
+
+    private String xiaoweiCode;
+
+    private String xiaoweiName;
+
+    /**
+     * 合同状态   0：已过期(不可抢入） 1：有效  2：作废（主动）
+     */
+    private String contractStatus;
+
+    /**
+     * 1.爆款产品   2.新需求 3.按照系列举单  4.按型号举单
+     */
+    private String contractType;
+
+    /**
+     * 合同用户群,1冰箱用户，2洗衣机用户
+     */
+    private String contractGroup;
+
+    /**
+     * 组织类型orgType,//1节点  2小微  3创客  4网格
+     */
+    private String orgType;
+
+    /**
+     * 上链关键字
+     */
+    private String hashCode;
+
+    private List<ZGamblingContractsProductIndex> indexList;
+
+
+}
