ef9c890732f5d9bbb63e86c9433d664c861bdd21
01431594 <S2LTshRZbaed1gk>
2019-12-19 10:12:26 +0800

    链群主抢单底线目标查询

---
+++ src/main/java/com/haier/hailian/contract/gambling/service/ZGamblingContractsProductIndexService.java
@@ -0,0 +1,16 @@
+package com.haier.hailian.contract.gambling.service;
+
+import com.haier.hailian.contract.entity.ZGamblingContractsProductIndex;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 01431594
+ * @since 2019-12-19
+ */
+public interface ZGamblingContractsProductIndexService extends IService<ZGamblingContractsProductIndex> {
+
+}
