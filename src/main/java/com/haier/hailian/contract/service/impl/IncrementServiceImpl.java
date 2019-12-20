90574496031a25f25254307a835ebd9696fe8ddc
HAIER\19033715 <liuyuqi@haier.com>
2019-12-20 14:51:18 +0800

    获取增值分享金额

--- src/main/java/com/haier/hailian/contract/service/impl/IncrementServiceImpl.java
+++ src/main/java/com/haier/hailian/contract/service/impl/IncrementServiceImpl.java
@@ -4,8 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.haier.hailian.contract.dao.ZContractsFactorDao;
 import com.haier.hailian.contract.dto.grab.CDGrabInfoSaveRequestDto;
 import com.haier.hailian.contract.entity.ZContractsFactor;
-import com.haier.hailian.contract.entity.ZSharePercent;
 import com.haier.hailian.contract.service.IncrementService;
+import com.haier.hailian.contract.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
@@ -24,7 +24,29 @@ public class IncrementServiceImpl implements IncrementService {
      */
     @Override
     public BigDecimal incrementMoney(CDGrabInfoSaveRequestDto requestDto) {
-        zContractsFactorDao.selectList(new QueryWrapper<ZContractsFactor>().eq("contract_id" , requestDto.getContractId()));
+        BigDecimal bottom = new BigDecimal(zContractsFactorDao.selectOne(new QueryWrapper<ZContractsFactor>()
+                .eq("contract_id" , requestDto.getContractId())
+                .eq("factor_code" , "")
+                .eq("factor_type" , Constant.FactorType.Bottom)).getFactorValue());
+
+        BigDecimal e2e = new BigDecimal(zContractsFactorDao.selectOne(new QueryWrapper<ZContractsFactor>()
+                .eq("contract_id" , requestDto.getContractId())
+                .eq("factor_code" , "")
+                .eq("factor_type" , Constant.FactorType.E2E)).getFactorValue());
+
+        BigDecimal grab = new BigDecimal(zContractsFactorDao.selectOne(new QueryWrapper<ZContractsFactor>()
+                .eq("contract_id" , requestDto.getContractId())
+                .eq("factor_code" , "")
+                .eq("factor_type" , Constant.FactorType.Grab)).getFactorValue());
+
+
+        BigDecimal money;
+
+
+//        if(){
+//
+//        }
+
         return null;
     }
 }
