48561e9cce13c729a332cdf87cec05d9a48da682
hesy <heshuying@haier.com>
2019-12-23 16:09:12 +0800

    链群目标未返回数据修改

--- src/main/java/com/haier/hailian/contract/dto/grab/CDGrabInfoResponseDto.java
+++ src/main/java/com/haier/hailian/contract/dto/grab/CDGrabInfoResponseDto.java
@@ -12,8 +12,6 @@ import lombok.Data;
 public class CDGrabInfoResponseDto {
     @ApiModelProperty(value="分享比例",name="分享比例",example="5%")
     private String sharePercent;
-    @ApiModelProperty(value="达成目标预计分享酬",name="达成目标预计分享酬",example="3000")
-    private String targetShareMoney;
     @ApiModelProperty(value="链群目标",name="链群目标",example="400")
     private String chainGoal;
     @ApiModelProperty(value="链群名称",name="链群名称")
