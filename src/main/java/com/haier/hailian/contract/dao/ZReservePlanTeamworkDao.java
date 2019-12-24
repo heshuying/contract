627dda73c776ffd6f1850a9d312b9657c1f604af
姜振东 <jiangzd102@outlook.com>
2019-12-24 18:58:34 +0800

    增加有关并联协同的接口。

---
+++ src/main/java/com/haier/hailian/contract/dao/ZReservePlanTeamworkDao.java
@@ -0,0 +1,80 @@
+package com.haier.hailian.contract.dao;
+
+import com.haier.hailian.contract.dto.ZReservePlanTeamworkDto;
+import com.haier.hailian.contract.entity.ZReservePlanTeamwork;
+import com.haier.hailian.contract.entity.ZReservePlanTeamworkDetail;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+/**
+ * (ZReservePlanTeamwork)表数据库访问层
+ *
+ * @author makejava
+ * @since 2019-12-24 17:32:41
+ */
+public interface ZReservePlanTeamworkDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    ZReservePlanTeamwork queryById(Integer id);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<ZReservePlanTeamwork> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param zReservePlanTeamwork 实例对象
+     * @return 对象列表
+     */
+    List<ZReservePlanTeamworkDto> queryAll(ZReservePlanTeamwork zReservePlanTeamwork);
+
+    /**
+     * 新增数据
+     *
+     * @param zReservePlanTeamwork 实例对象
+     * @return 影响行数
+     */
+    int insert(ZReservePlanTeamwork zReservePlanTeamwork);
+
+    /**
+     * 修改数据
+     *
+     * @param zReservePlanTeamwork 实例对象
+     * @return 影响行数
+     */
+    int update(ZReservePlanTeamwork zReservePlanTeamwork);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 影响行数
+     */
+    int deleteById(Integer id);
+
+
+    int selectCountByParentId(Integer parentId);
+
+    /**
+     * 保存数据
+     * @param zReservePlanTeamworkDto
+     * @return
+     */
+    int save(ZReservePlanTeamworkDto zReservePlanTeamworkDto);
+
+
+    int insertDetail(ZReservePlanTeamworkDetail zReservePlanTeamworkDetail);
+
+}
---
+++ src/main/java/com/haier/hailian/contract/dto/ZReservePlanTeamworkDto.java
@@ -0,0 +1,56 @@
+package com.haier.hailian.contract.dto;
+
+import com.haier.hailian.contract.entity.ZReservePlanTeamwork;
+import com.haier.hailian.contract.entity.ZReservePlanTeamworkDetail;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * (ZReservePlanTeamwork)实体类
+ *
+ * @author makejava
+ * @since 2019-12-24 17:13:55
+ */
+@Data
+@ApiModel(value = "ZReservePlanTeamworkDto", description = "并联交互预案")
+public class ZReservePlanTeamworkDto implements Serializable {
+    private static final long serialVersionUID = 992358289080385319L;
+    //主键
+    private Integer id;
+    //合约id
+    private Integer parentId;
+    //问题编码
+    private String problemCode;
+    //问题描述
+    private String problemContent;
+    //开始时间
+    private String startTime;
+    //结束时间
+    private String endTime;
+    //提醒时间
+    private String remindTime;
+    //是否重复：0/1 - 是/否
+    private String isRepeat;
+    //是否重要：0/1 - 是/否
+    private String isImportant;
+    //执行者
+    private String executer;
+    //协同者
+    private String teamworker;
+    //创建者编码
+    private String createUserCode;
+    //创建者名称
+    private String createUserName;
+    //创建时间
+    private Date createUserTime;
+    //任务编码
+    private String taskCode;
+
+
+    List<ZReservePlanTeamworkDetail> details;
+
+}
