75f97bef31fad5291d06650def66ee701fc2366a
01431594 <S2LTshRZbaed1gk>
2019-12-30 12:58:31 +0800

    合约列表

--- src/main/java/com/haier/hailian/contract/dao/ZContractsDao.java
+++ src/main/java/com/haier/hailian/contract/dao/ZContractsDao.java
@@ -54,5 +54,17 @@ public interface ZContractsDao extends BaseMapper<ZContracts> {
      */
     List<ContractViewDataCD> selectContractsViewForCD(String contractId);
 
+    /**
+     * 查询已抢入合约列表
+     * @param queryDTO
+     * @return
+     */
     List<ZContracts> selectMyGrabContract(QueryContractListDTO queryDTO);
+
+    /**
+     * 查询待抢入合约列表
+     * @param queryDTO
+     * @return
+     */
+    List<ZContracts> selectToGrabContract(QueryContractListDTO queryDTO);
 }
