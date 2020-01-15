a5b0aa059ed0db6e932d323a454534572976f05c
01431594 <S2LTshRZbaed1gk>
2020-01-15 15:54:40 +0800

    首页合约列表接口

---
+++ src/main/java/com/haier/hailian/contract/dto/QueryContractListDTO2.java
@@ -0,0 +1,13 @@
+package com.haier.hailian.contract.dto;
+
+import lombok.Data;
+
+/**
+ * Created by 01431594 on 2020/1/15.
+ */
+@Data
+public class QueryContractListDTO2 {
+
+    private String month;
+    private String userCode;
+}
