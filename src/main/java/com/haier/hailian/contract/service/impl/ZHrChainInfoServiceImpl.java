7f4e633df981a0b46b03b99569e80d633e75e29f
19012964 <panyulong@haier.com>
2020-01-20 10:03:50 +0800

    【fix】add prod config of dubbo

--- src/main/java/com/haier/hailian/contract/service/impl/ZHrChainInfoServiceImpl.java
+++ src/main/java/com/haier/hailian/contract/service/impl/ZHrChainInfoServiceImpl.java
@@ -40,8 +40,8 @@ public class ZHrChainInfoServiceImpl implements ZHrChainInfoService {
     @Resource
     private ZNodeTargetPercentInfoDao zNodeTargetPercentInfoDao;
     //hr发版后放开
-//    @Reference(version = "ehr2.0", registry = "registry2", check = false)
-    @Reference(version = "ehr2.0-test",registry = "registry2",check=false)
+    @Reference(version = "ehr2.0", registry = "registry2", check = false)
+    //@Reference(version = "ehr2.0-test",registry = "registry2",check=false)
     ChainGroupClient chainGroupClient;
 
     /**
