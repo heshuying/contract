c7aa51cf0a2f2828b82f33d8a653dcab51cff3e3
hesy <heshuying@haier.com>
2020-03-06 14:36:25 +0800

    抢单页面增加复核字段

--- src/main/java/com/haier/hailian/contract/dto/grab/CDGrabInfoResponseDto.java
+++ src/main/java/com/haier/hailian/contract/dto/grab/CDGrabInfoResponseDto.java
@@ -34,5 +34,7 @@ public class CDGrabInfoResponseDto {
     private String xwName;
     @ApiModelProperty(value="增值分享比例",name="增值分享比例",example="1%")
     private String zzfxRate;//增值分享比例（新增字段）
+    @ApiModelProperty(value="链群主是否已复核",name="链群主是否已复核",example="true")
+    private String isReviewed = "false";
     private List<CDGrabTargetDto> targetList = new ArrayList<>();
 }
