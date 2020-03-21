050de6e52b6d92b6ef530efbcc76f721b96ca927
hesy <heshuying@haier.com>
2020-03-21 21:13:23 +0800

    查看进度页面pc端需求调整

--- src/main/java/com/haier/hailian/contract/dto/CDGrabDataDTO.java
+++ src/main/java/com/haier/hailian/contract/dto/CDGrabDataDTO.java
@@ -2,6 +2,7 @@ package com.haier.hailian.contract.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,9 +16,13 @@ public class CDGrabDataDTO {
     private String xwCode;
     private String xwName;
     private String sharePercent;
+    /** 复核按钮可用标志*/
+    private String checkEnable = "true";
+    /** 踢出按钮可用标志*/
+    private String kickoutEnable = "true";
 
     /** 目标列表*/
-    private List<TargetViewDTO> targetList;
+    private List<TargetViewDTO> targetList = new ArrayList<>();
     /** 预案列表*/
-    private List<ReservePlanDetailDTO> planList;
+    private List<ReservePlanDetailDTO> planList = new ArrayList<>();
 }
