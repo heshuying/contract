2a6e5fd02812da304f3b7b1f8d7f71e8bc5383e7
HAIER\19033715 <liuyuqi@haier.com>
2020-04-07 17:18:04 +0800

    对外增加链群其他信息

--- src/main/java/com/haier/hailian/contract/dto/homepage/ChainDataInfo.java
+++ src/main/java/com/haier/hailian/contract/dto/homepage/ChainDataInfo.java
@@ -1,7 +1,10 @@
 package com.haier.hailian.contract.dto.homepage;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class ChainDataInfo {
@@ -19,4 +22,26 @@ public class ChainDataInfo {
     private String masterName;
 
     private String chainPtCode;
+
+    //是否删除
+    private int deleted;
+
+    //群组ID
+    private String groupId;
+
+    //定位
+    private String fixedPosition;
+
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createDate;
+
+    private String zzfxRate;//增值分享比例
+
+    private String cdShareRate; // 创单分享比例
+
+    private String tyShareRate; // 体验分享比例
+
+    private String parentCode; //主链群编码
+
+    private String grabFlag; // 是否在oms抢单
 }
